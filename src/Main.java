96bd3101157f09ec488c60fbd2f9e3932601aaca
Sergey Nazarov <ric19902002@icloud.com>
2025-08-19 19:22:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("введите число для расчета факториала");
+        System.out.println("Enter a number to calculate its factorial:");
         int factorial = 1;
         int value = new Scanner(System.in).nextInt();
-        String massage = value + "! = ";
+        String message = value + "! = ";
+
         switch (value) {
-            case 0 -> System.out.println("не является натуральным числом");
-            case 1 -> System.out.println(massage + value);
-            case 2 -> System.out.println(massage + "1 * " + value + " = " + value);
+            case 0 -> System.out.println("0 is not a natural number");
+            case 1 -> System.out.println(message + value);
+            case 2 -> System.out.println(message + "1 * " + value + " = " + value);
             default -> {
                 if (value < 0) {
-                    System.out.println("не является натуральным числом");
+                    System.out.println("Negative numbers are not natural numbers");
                     break;
                 }
-                System.out.print(massage);
-                for (int i = 1; i <= value; i = i + 1) {
+                System.out.print(message);
+                for (int i = 1; i <= value; i++) {
                     factorial = factorial * i;
                     System.out.print(i);
                     if (i < value) {
